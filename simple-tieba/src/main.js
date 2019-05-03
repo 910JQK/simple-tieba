67470108dabc5aa30433ff97a073cfc06fba73a6
910JQK <v910JQK@gmail.com>
2019-05-03 20:51:14 +0800

    fix

--- simple-tieba/src/main.js
+++ simple-tieba/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import { JSDOM } from 'jsdom'
+import 'whatwg-fetch'
+
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import 'material-design-icons/iconfont/material-icons.css'
+
 import App from './App.vue'
+import router from './router'
+import Navigation from 'vue-navigation'
+
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
+Vue.use(Navigation, { router })
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+var vm = new Vue({ router, render: h => h(App) })
+vm.$mount('#app')
--- simple-tieba/vue.config.js
+++ simple-tieba/vue.config.js
@@ -1,5 +1,12 @@
 const path = require("path");
+const WriteFilePlugin = require('write-file-webpack-plugin')
+
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../www")
+    outputDir: path.resolve(__dirname, "../www"),
+    configureWebpack: {
+        plugins: [
+            new WriteFilePlugin()
+        ],
+    }
 }
